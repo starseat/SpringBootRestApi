25a588489719634ef6a1d5780ed55b1511f4bb7c
starseat <starseat3@gmail.com>
2021-06-21 13:20:56 +0900

    프로젝트 최초 생성

---
+++ BootApiSample/src/test/java/com/rest/bootapisample/BootApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.bootapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApiSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
